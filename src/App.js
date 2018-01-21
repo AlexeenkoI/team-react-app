c8bc28dbe50c2ffa61420250e9c84dd04535451d
Igor Alekseenko <fastedx@gmail.com>
2018-01-21 20:56:27 +0300

    create app

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+//import styles from './styles/font-awesome'
+
+class App extends React.Component {
+ render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to my-React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
